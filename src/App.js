cf5689aa440df96edb94ae835ad0fb8dcf4c066e
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-20 21:55:14 +0100

    Add yt-key and refactor App to class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 import './style/App.css';
 import SearchBar from './components/SearchBar';
 
-const App = () => (
-  <SearchBar />
-);
+const API_KEY = 'AIzaSyAsLjJsnSSVgwMXXosvN5HXCOdcELma2gM';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+  console.log(data);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
 
 export default App;
