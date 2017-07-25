a7511ac8a416b2be4802212632a91fda6f4cc670
Jonny Pickard <jonathandpickard@gmail.com>
2017-07-25 12:47:44 +0100

    Add selected video state to App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import YTSearch from 'youtube-api-search';
 import './style/App.css';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyAsLjJsnSSVgwMXXosvN5HXCOdcELma2gM';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { videos: [] };
+
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
@@ -20,11 +28,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
   }
 }
-
 
 export default App;
--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -3,10 +3,22 @@ import { PropTypes } from 'prop-types';
 
 const VideoListItem = ({ video }) => {
   const { snippet } = video;
-  const { title } = snippet;
+  const { title, thumbnails } = snippet;
+  const { url } = thumbnails.default;
 
   return (
-    <li>{title}</li>
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={url} alt="" />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{title}</div>
+        </div>
+      </div>
+
+    </li>
   );
 };
 
